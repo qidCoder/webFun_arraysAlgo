065873434836cbbea322d6729bea1022a58a55e4
qidCoder <qidcoder@gmail.com>
2020-09-21 08:50:31 -0400

    Create arraysAlgo.js

---
+++ arraysAlgo.js
@@ -0,0 +1,42 @@
+//Created by Shelley Ophir
+//Coding Dojo Sep. 21,2020
+//Create an algorithm for each of the challenges below
+
+//Using the given array:
+
+var testArr = [6,3,5,1,2,4]
+
+//Print Values and Sum
+
+//Print each array value and the sum so far
+//The expected output will be: 
+//Num 6, Sum 6
+//Num 3, Sum 9
+//Num 5, Sum 14
+//Num 1, Sum 15
+//Num 2, Sum 17
+//Num 4, Sum 21
+
+var sum = 0;
+
+for (var i = 0; i < testArr.length; i++)
+{
+  sum += testArr[i];
+  
+  console.log("Num: " + testArr[i] + ", Sum: " + sum);
+}
+
+//////////////////////////////////////////////
+
+//Value * Position
+
+//Multiply each value in the array by its array position
+//The expected output will be:
+//[0,3,10,3,8,20]
+
+for (var i = 0; i < testArr.length; i++)
+{
+  testArr[i] = testArr[i] * i;
+}
+
+console.log(testArr);
